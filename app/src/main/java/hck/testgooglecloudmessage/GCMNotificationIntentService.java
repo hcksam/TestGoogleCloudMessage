33820f3efe10704af8f9d0c1f0026034084e780f
hcksam <hcksam@hotmail.com>
2016-04-29 18:27:05 +0800

    update

--- app/src/main/java/hck/testgooglecloudmessage/GCMNotificationIntentService.java
+++ app/src/main/java/hck/testgooglecloudmessage/GCMNotificationIntentService.java
@@ -31,23 +31,29 @@ public class GCMNotificationIntentService extends IntentService {
         String messageType = gcm.getMessageType(intent);
 
         if (!extras.isEmpty()) {
+            String showMessage = null;
+            String receiveMessage = null;
             if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR
                     .equals(messageType)) {
-                sendNotification("Send error: " + extras.toString());
+                showMessage = "Send error: ";
+                receiveMessage = extras.toString();
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED
                     .equals(messageType)) {
-                sendNotification("Deleted messages on server: "
-                        + extras.toString());
+                showMessage = "Deleted messages on server: ";
+                receiveMessage = extras.toString();
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE
                     .equals(messageType)) {
-                sendNotification("Message Received from Google GCM Server:nn"
-                        + extras.get(ApplicationConstants.MSG_KEY));
+                showMessage = "Message Received from Google GCM Server:\n\n";
+                receiveMessage = extras.get(ApplicationConstants.MSG_KEY).toString();
+            }
+            if (receiveMessage != null){
+                sendNotification(showMessage+receiveMessage, receiveMessage);
             }
         }
         GcmBroadcastReceiver.completeWakefulIntent(intent);
     }
 
-    private void sendNotification(String msg) {
+    private void sendNotification(String msg, String receiveMessage) {
         Intent resultIntent = new Intent(this, HomeActivity.class);
         resultIntent.putExtra("msg", msg);
         PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0,
@@ -72,8 +78,9 @@ public class GCMNotificationIntentService extends IntentService {
         defaults = defaults | Notification.DEFAULT_SOUND;
 
         mNotifyBuilder.setDefaults(defaults);
+        mNotifyBuilder.setContentTitle("New message from Server");
         // Set the content for Notification
-        mNotifyBuilder.setContentText("New message from Server");
+        mNotifyBuilder.setContentText(receiveMessage);
         // Set autocancel
         mNotifyBuilder.setAutoCancel(true);
         // Post a notification
